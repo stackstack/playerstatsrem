4f9358549cd47646b5944638d974751e2ce03bc2
stackstack <stackstackmelnychuk@users.noreply.github.com>
2019-10-23 15:16:14 +0300

    Updates

---
+++ precache-manifest.e9e39ff1d6cf6e3e3f0b4e1bcf81dd66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3286ea45bc45954fe9b52b78f5792a",
+    "url": "/playerstatsrem/index.html"
+  },
+  {
+    "revision": "dbd79056e731776ef7de",
+    "url": "/playerstatsrem/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0f5b885de745fec080e8",
+    "url": "/playerstatsrem/static/js/2.35b4b7d0.chunk.js"
+  },
+  {
+    "revision": "dbd79056e731776ef7de",
+    "url": "/playerstatsrem/static/js/main.cd0d5835.chunk.js"
+  },
+  {
+    "revision": "2081aed401c5c5262ea4",
+    "url": "/playerstatsrem/static/js/runtime-main.0f3219f8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/playerstatsrem/static/media/logo.25bf045c.svg"
+  }
+]);
